a48ed1f9747bc91cd4dafc8be268ab14964ca37f
AdrianGL <55089389+GlAdrian-22@users.noreply.github.com>
2020-11-26 20:00:19 -0600

    crud

---
+++ scripts.php
@@ -0,0 +1,15 @@
+
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="lib/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="lib/datatable/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="lib/datatable/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" type="text/css" href="lib/alertify/css/alertify.css">
+<link rel="stylesheet" type="text/css" href="lib/alertify/css/themes/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="lib/fontawesome/css/font-awesome.css">
+
+<script src="lib/jquery.min.js"></script>
+<script src="lib/bootstrap/popper.min.js"></script>
+<script src="lib/bootstrap/bootstrap.min.js"></script>
+<script src="lib/datatable/jquery.dataTables.min.js"></script>
+<script src="lib/datatable/dataTables.bootstrap4.min.js"></script>
+<script src="lib/alertify/alertify.js"></script>
